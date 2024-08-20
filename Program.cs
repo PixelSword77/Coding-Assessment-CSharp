f7bca3bc795ca6561bd247d4bc72ba6f098858b8
Nicholas <141095727+PixelSword77@users.noreply.github.com>
2024-08-20 15:10:47 -0700

    Database connection established
    A new script and object type has been created to house the connection to the database. Connection is established in main and will be passed to future functions.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
-﻿class Program
+﻿using Coding_Assessment_CSharp.scripts.objects;
+
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello world!");
+        Console.WriteLine("#############################");
+        Console.WriteLine("Initializing...");
 
-        // Initialize the connection to the database
+        // Initialize the connection to the database, exit if the connection fails
+        DatabaseConnection dbConnection = new DatabaseConnection("localhost", "username", "password", "products");
+        if(!dbConnection.ConnectToDatabase())
+        {
+            Console.WriteLine("Database connection failed, exiting program.");
+            Environment.Exit(0);
+            return;
+        }
 
         // Retrieve the list of products and store them in an object
+
 
         // Print a table to the console showing the available products, codes, and prices
 
@@ -23,5 +34,7 @@
         // Calculate total
 
         // Display total
+
+        Console.ReadKey();
     }
 }
---
+++ scripts/objects/DatabaseConnection.cs
@@ -0,0 +1,71 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Assessment_CSharp.scripts.objects
+{
+    internal class DatabaseConnection
+    {
+        public DatabaseConnection(string server, string user, string password, string database)
+        {
+            ServerName = server;
+            UserID = user;
+            Password = password;
+            DatabaseName = database;
+        }
+
+        string ServerName = "";
+        string UserID = "";
+        string Password = "";
+        string DatabaseName = "";
+        MySqlConnection connection;
+
+        public bool ConnectToDatabase()
+        {
+            bool connected = false;
+
+            string connectionString = "Server=" + ServerName + ";User ID=" + UserID + ";Password=" + Password + ";Database=" + DatabaseName;
+
+            MySqlConnection newConnection = new MySqlConnection(connectionString);
+            newConnection.Open();
+            Console.WriteLine("Attempting new connection to database: " + DatabaseName);
+
+            int dotCount = 0;
+            int connectionTime = 0;
+            while (newConnection.State == ConnectionState.Connecting)
+            {
+                Console.WriteLine("\rTrying to connect" + new string('.', dotCount));
+                Task.Delay(10000);
+
+                dotCount++;
+                if (dotCount > 3)
+                    dotCount = 0;
+
+                connectionTime++;
+                if (connectionTime > 1000)
+                    break;
+            }
+
+            switch (newConnection.State)
+            {
+                case ConnectionState.Open:
+                    Console.WriteLine("Successfully connected to database!");
+                    connected = true;
+                    break;
+                case ConnectionState.Closed:
+                    Console.WriteLine("Connection to database closed.");
+                    break;
+                case ConnectionState.Connecting:
+                    Console.WriteLine("Connection took too long and timed out.");
+                    break;
+            }
+
+            return connected;
+        }
+    }
+}
